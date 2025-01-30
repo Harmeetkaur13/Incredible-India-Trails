1633bf4ce454d0972c16faeba39b20211f14a1aa
Harmeetkaur13 <iharmeetk@gmail.com>
2025-01-30 21:14:36 +0000

    added search bar ,filter by category code to refine destination results

--- destination/urls.py
+++ destination/urls.py
@@ -4,5 +4,4 @@ from django.urls import path
 urlpatterns = [
     path('', views.AllDestinations.as_view(), name='home'),
     path('<str:name>/', views.destination_detail, name='view_destination'),
-    path('category/<str:category_name>/', views.filter_by_category, name='filter_by_category'),
 ]
--- destination/views.py
+++ destination/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.db.models import Q
 from .models import Category,Destination
 
 # Create your views here.
 class AllDestinations(generic.ListView):
-    queryset = Destination.objects.all()
     template_name = "destination/Homepage.html"
     paginate_by = 6
+
+    def get_queryset(self):
+        queryset = Destination.objects.all()
+        search_query = self.request.GET.get('search', '')
+        category_id = self.request.GET.get('category', '')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(category__name__icontains=search_query) |
+                Q(location__icontains=search_query) |
+                Q(added_by__username__icontains=search_query)
+            )
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
@@ -34,10 +52,3 @@ def destination_detail(request, name):
         "destination/view_destination.html",
         {"destination": destination},
     )
-def filter_by_category(request, category_name):
-    category = get_object_or_404(Category, name=category_name)
-    destinations = Destination.objects.filter(category=category)
-    return render(request, "filter_by_category.html", {
-        "category": category,
-        "destinations": destinations
-    })
