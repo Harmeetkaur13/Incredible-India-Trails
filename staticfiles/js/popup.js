4a177826d0bdc21410c36e488ebec25461fef172
Harmeetkaur13 <iharmeetk@gmail.com>
2025-02-05 16:42:50 +0000

    collect staticfiles

--- staticfiles/js/popup.js
+++ staticfiles/js/popup.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
             updateStarRating(value);
         });
     });
-    const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+    const deleteModal = (document.getElementById("deleteModal"));
     const deleteButtons = document.getElementsByClassName("btn-delete");
     const deleteConfirm = document.getElementById("deleteConfirm");
     /**
@@ -76,5 +76,22 @@ document.addEventListener("DOMContentLoaded", function() {
             updateStarRating(starCount);
         });
     }
-});    
+    // Pop-up message functionality
+    var popup = document.getElementById("popup-message");
+    var overlay = document.getElementById("overlay");
+
+    if (popup) {
+        popup.style.display = "block";
+        overlay.style.display = "block";
+    }
+
+    var popupClose = document.getElementById("popup-close");
+    if (popupClose) {
+        popupClose.addEventListener("click", closePopup);
+    }
+});
+function closePopup() {
+    document.getElementById("popup-message").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
     
